521d401a07a81c1a4650497ef4088b62aa00ae3a
0xakash9 <devakash256@gmail.com>
2024-06-14 23:55:00 +0530

    Q283

--- go/main.go
+++ go/main.go
@@ -3,24 +3,40 @@ package main
 import "fmt"
 
 // Remove Element
-func main (){
- RemoveELement()
+func main() {
+	RemoveELement()
+	moveZeros()
 }
 
+func RemoveELement() {
+	var nums = [...]int{3, 2, 3, 3}
+	var val = 3
 
+	var k = 0
 
-func RemoveELement(){
-	var nums=[...]int{3,2,3,3}
-    var val=3
-
-	var k=0;
-
-	for i:=0; i < len(nums); i++ {
-      if nums[i] != val{
-		nums[k] = nums[i];
-		k++
-	  }
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[k] = nums[i]
+			k++
+		}
 	}
 
-	fmt.Println("k",k)
+	fmt.Println("k", k)
+}
+
+func moveZeros() {
+	var nums = [...]int{0, 1, 0, 3, 12}
+
+	var k = 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			var copyVal = nums[k]
+			nums[k] = nums[i]
+			nums[i] = copyVal
+			k++
+		}
+	}
+
+	fmt.Println("nums", nums)
 }
