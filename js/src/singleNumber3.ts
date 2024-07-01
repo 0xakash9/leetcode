a09bc668876065265d3ec40d532412aee9ee4fd8
0xakash9 <devakash256@gmail.com>
2024-07-01 14:07:58 +0530

    Q260

---
+++ js/src/singleNumber3.ts
@@ -0,0 +1,16 @@
+function singleNumber(nums: number[]): number[] {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      const val = map.get(nums[i]);
+      map.set(nums[i], val + 1);
+    } else {
+      map.set(nums[i], 1);
+    }
+  }
+
+  const el = Array.from(map).filter(([key, val]) => val === 1);
+
+  return el.map((i) => i[0]);
+}
